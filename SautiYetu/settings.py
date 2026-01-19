bfac54608a331b8a47996b1076e63fee6bbc14ee
antonie <antonriziki@gmail.com>
2026-01-19 20:48:26 +0300

    added the whitenoise runserver_nostatic method to the django settings

--- SautiYetu/settings.py
+++ SautiYetu/settings.py
@@ -25,7 +25,7 @@ ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
